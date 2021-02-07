7879f730bc82149c179305fbec1799001e3d7d35
bikashpaulbp <bikashpaulbp92@gmail.com>
2021-02-07 10:29:17 +0600

    done up to foreach

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=b')
-.then(res => res.json())
-.then(data => console.log(data))
+
+document.getElementById("search-btn").addEventListener("click", function () {
+    const getLink = document.getElementById("meal-input").value;
+    const link = `https://www.themealdb.com/api/json/v1/1/search.php?f=${getLink}`
+    console.log(link)
+
+    fetch(link)
+        .then(res => res.json())
+        .then(data => displayMeal(data.meals))
+
+
+    function displayMeal(data) {
+        const mealNames = data.map(data => data.strMeal)
+        const mealImages = data.map(data => data.strMealThumb)
+        mealNames.forEach(meal => {
+            const mealName = meal;
+            const imageLink = meal;
+            console.log(mealName)
+
+            const parentDiv = document.getElementById("main-div");
+            const childDiv = document.createElement("div")
+            parentDiv.appendChild(childDiv);
+
+            childDiv.style.width = "250px";
+            childDiv.style.height = "350px";
+            childDiv.style.border = "1px solid gray";
+            childDiv.style.margin = "10px 10px";
+            childDiv.style.borderRadius = "10px";
+            childDiv.style.boxShadow = "5px 5px 10px green";
+            childDiv.style.cursor = "pointer";
+
+            const mealImage = document.createElement("img");
+            mealImage.setAttribute('src', imageLink);
+            mealImage.style.width = "200px";
+            mealImage.style.height = "150px";
+            mealImage.style.paddingTop = "10px";
+            mealImage.style.paddingBottom = "10px";
+            childDiv.appendChild(mealImage);
+
+        });
+
+
+        
+    }
+})
 
