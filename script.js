9d9ba5b224cabd1a0364d4e4e5f8186c245d353e
bikashpaulbp <bikashpaulbp92@gmail.com>
2021-02-07 11:02:10 +0600

    done

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.getElementById("search-btn").addEventListener("click", function () {
     const getLink = document.getElementById("meal-input").value;
     const link = `https://www.themealdb.com/api/json/v1/1/search.php?f=${getLink}`
-    console.log(link)
+    
 
     fetch(link)
         .then(res => res.json())
@@ -10,19 +10,23 @@ document.getElementById("search-btn").addEventListener("click", function () {
 
 
     function displayMeal(data) {
-        const mealNames = data.map(data => data.strMeal)
-        const mealImages = data.map(data => data.strMealThumb)
-        mealNames.forEach(meal => {
-            const mealName = meal;
-            const imageLink = meal;
-            console.log(mealName)
+        const meals = data.map(data => data)
+        // const mealNames = data.map(data => data.strMeal)
+        // const mealImages = data.map(data => data.strMealThumb)
+        console.log(meals);
+        meals.forEach(meal => {
+            const mealName = meal.strMeal;
+            const imageLink = meal.strMealThumb;
+
+            
+            
 
             const parentDiv = document.getElementById("main-div");
             const childDiv = document.createElement("div")
             parentDiv.appendChild(childDiv);
 
             childDiv.style.width = "250px";
-            childDiv.style.height = "350px";
+            childDiv.style.height = "200px";
             childDiv.style.border = "1px solid gray";
             childDiv.style.margin = "10px 10px";
             childDiv.style.borderRadius = "10px";
@@ -35,6 +39,7 @@ document.getElementById("search-btn").addEventListener("click", function () {
             mealImage.style.height = "150px";
             mealImage.style.paddingTop = "10px";
             mealImage.style.paddingBottom = "10px";
+            mealImage.style.paddingLeft = "20px"
             childDiv.appendChild(mealImage);
 
         });
